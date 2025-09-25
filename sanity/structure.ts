2c1aa70c527269305b1fe12ab2e5227140815631
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-25 15:53:44 -0400

    added sanity schema files for product, category, sale, order types

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,5 +3,11 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('Shopr')
+    .items([
+      S.documentTypeListItem("category").title("Categories"),
+      S.divider(),
+      ...S.documentTypeListItems().filter(
+        item => item.getId() && !["post", "category"].includes(item.getId()!)
+      )
+    ])
