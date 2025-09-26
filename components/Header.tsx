1a7b87f76cf28ab335a22082ebb75565bedfd8c4
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-26 17:09:02 -0400

    added basket checkout page with stripe payment functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,13 @@ import { ClerkLoaded, SignedIn, SignInButton, UserButton, useUser } from "@clerk
 import Link from "next/link"
 import Form from "next/form"
 import { PackageIcon, TrolleyIcon } from "@sanity/icons"
+import useBasketStore from "@/store/store"
 
 export default function Header() {
   const { user } = useUser()
+  const itemCount = useBasketStore(state =>
+    state.items.reduce((total, item) => total + item.quantity, 0)
+  )
 
   const createClerkPasskey = async () => {
     try {
@@ -43,7 +47,10 @@ export default function Header() {
               bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               <TrolleyIcon className="w-6 h-6" />
-              {/* span item count once global state is implemented */}
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {itemCount}
+              </span>
+
               <span>My Basket</span>
             </Link>
             <ClerkLoaded>
