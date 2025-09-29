ddb4bfd78aceca259c8968109fd27a6b7d1c5714
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-09-29 18:13:38 -0400

    middleware bug nodejs force

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,20 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next|.*\\..*).*)',
     '/(api|trpc)(.*)',
   ],
-}
+  runtime: "nodejs", // 👈 Force Node runtime instead of Edge
+};
+
+export default clerkMiddleware()
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
